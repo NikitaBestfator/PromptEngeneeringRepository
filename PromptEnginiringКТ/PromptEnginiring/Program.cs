35ae8863a8a4d30acaa7678a6f0b099fdb0f3344
nbestfator@gmail.com <nbestfator@gmail.com>
2025-11-04 23:06:02 +0300

    Задание по PromptEngineering

--- PromptEnginiringКТ/PromptEnginiring/Program.cs
+++ PromptEnginiringКТ/PromptEnginiring/Program.cs
@@ -9,13 +9,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        CategoriseWords();
+        CheckTriggerWords();
         return;
+        CategoriseWords();
         CompareTexts();
         Console.WriteLine();
         Console.WriteLine("==========================");
         Console.WriteLine();
         CompareWords();
+        CheckTriggerWords();
     }
 
     private static HashSet<string> LoadWordsFromFile(string filePath)
@@ -23,18 +25,35 @@ class Program
         string text = File.ReadAllText(filePath);
         var words = ExtractWords(text);
         return words;
-        
+
     }
-    
+
     static Regex Email = new Regex(@".+@(mail\.ru|gmail\.com|spb\.ithub\.ru|yandex\.ru)");
 
-    static Regex MathFunction = new Regex(@"[\+=\-\*\/]{1}");   
-        
+    static Regex MathFunction = new Regex(@"[\+=\-\*\/]{1}");
+
+    static Regex Words = new Regex(@"^[a-zA-Zа-яА-Я]+$");
+
     static HashSet<string> ExtractWords(string text)
     {
-        return new HashSet<string>(text.Split([' ', '\n', '\r'], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)); 
+        return text.Split(
+            [' ', '\n', '\r'],
+            StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(i=>CleanWord(i))
+            .ToHashSet();
     }
+
+    private const string CleanGroup = @"\.\(,:!";
+    static Regex Cleaner = new Regex($"^[{CleanGroup}]*(?<word>[^{CleanGroup}]+)[{CleanGroup}]*$");
     
+    private static String CleanWord(string s)
+    {
+        var m = Cleaner.Match(s).Groups["word"];
+        if(m.Success) return m.Value;
+
+        return s;
+    }
+
     static void CategoriseWords()
     {
         string filePath = "file3.txt";
@@ -50,19 +69,25 @@ class Program
             {
                 Console.WriteLine($"математическая функция");
             }
-            else
+            else if (Words.IsMatch(word))
             {
                 Console.WriteLine($"просто слово");
             }
+            else
+            {
+                Console.WriteLine($"прочие");
+            }
         }
-        
+
     }
+
     static void CompareTexts()
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("ЗАДАНИЕ: 1. Входящие два текстовых файла, в которых какое-то кол-во слов, есть ли какое-то слово из второго файла в первом, реализация любая от true or false до кол-ва слов найденных в обоих файлах. КОД в CSharp");
+        Console.WriteLine(
+            "ЗАДАНИЕ: 1. Входящие два текстовых файла, в которых какое-то кол-во слов, есть ли какое-то слово из второго файла в первом, реализация любая от true or false до кол-ва слов найденных в обоих файлах. КОД в CSharp");
         Console.ResetColor();
-        
+
         string file1Path = "file1.txt";
         string file2Path = "file2.txt";
 
@@ -94,25 +119,26 @@ class Program
             Console.WriteLine("Ошибка при чтении файлов или обработке данных:");
             Console.WriteLine(ex.Message);
         }
-        
+
     }
-    
+
     static void CompareWords()
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("ЗАДАНИЕ: 2. Входящие два файла, каждый с одним словом, проверить на сколько процентов они совпадают. КОД в CSharp");
+        Console.WriteLine(
+            "ЗАДАНИЕ: 2. Входящие два файла, каждый с одним словом, проверить на сколько процентов они совпадают. КОД в CSharp");
         Console.ResetColor();
-        
+
         string file1Path = "word1.txt";
         string file2Path = "word2.txt";
-    
+
         try
         {
             // Читаем содержимое файлов
             // Извлекаем слова, приводим к нижнему регистру
             var words1 = LoadWordsFromFile(file1Path);
             var words2 = LoadWordsFromFile(file2Path);
-            
+
             if (words1.Count != words2.Count && words1.Count != 1)
             {
                 throw new InvalidDataException("Проверьте число слов в файлах!");
@@ -136,12 +162,12 @@ class Program
             {
                 Console.WriteLine("Совпадающих слов нет.");
             }
-            
+
             double x1 = (1.0 * commonChars.Count) / w1.Count();
-            double x2 =  (1.0 * commonChars.Count) / w2.Count();
+            double x2 = (1.0 * commonChars.Count) / w2.Count();
             Console.WriteLine($"Первое слово совпадает со вторым на {x1:P}!!");
             Console.WriteLine($"Второе слово совпадает с первым на {x2:P}!!");
-            
+
         }
         catch (Exception ex)
         {
@@ -149,5 +175,62 @@ class Program
             Console.WriteLine(ex.Message);
         }
     }
-    
+
+    static void CheckTriggerWords()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(
+            "ЗАДАНИЕ: 3. На вход подаются TriggerWords.txt и Text.txt, если есть хотя-бы 85 процентов слов, совпадающих с Text.txt. И определить % соотношение типов слов. Есть слово, математическая функция, емейл и прочие.");
+        Console.ResetColor();
+        string file4Path = "TriggerWords.txt";
+        string file5Path = "text.txt";
+
+        var triggerWords = LoadWordsFromFile(file4Path);
+        var words = LoadWordsFromFile(file5Path);
+        var commonWords = words.Intersect(triggerWords).ToList();
+
+        double x1 = commonWords.Count * 100.0 / words.Count;
+        if (x1 < 85)
+        {
+            Console.WriteLine($"Процент совпавших слов меньше 85:{x1}% - выходим");
+            return;
+        }
+
+        int emailsCount = 0;
+        int mathFunctionsCount = 0;
+        int wordsCount = 0;
+        int otherWordsCount = 0;
+
+        foreach (var word in words)
+        {
+            if (Email.IsMatch(word))
+            {
+                emailsCount++;
+            }
+            else if (MathFunction.IsMatch(word))
+            {
+                mathFunctionsCount++;
+            }
+            else if (Words.IsMatch(word))
+            {
+                wordsCount++;
+            }
+            else
+            {
+                otherWordsCount++;
+            }
+        }
+        
+        double emailsProcents = emailsCount * 1.0 / words.Count;
+        double  mathFunctionsProcents = mathFunctionsCount * 1.0 / words.Count;
+        double wordsProcents = wordsCount * 1.0 / words.Count;
+        double otherWordsProcents = otherWordsCount * 1.0 / words.Count;
+        
+        Console.WriteLine($"{"emailsProcents",-25} -> {emailsProcents,10:P}");
+        Console.WriteLine($"{"mathFunctionsProcents",-25} -> {mathFunctionsProcents,10:P}");
+        Console.WriteLine($"{"wordsProcents",-25} -> {wordsProcents,10:P}");
+        Console.WriteLine($"{"otherWordsProcents",-25} -> {otherWordsProcents,10:P}");
+        
+        
+    }
 }
